9a4218afe7065297ec7b66c9e00c1487e425a45b
James <160443991+jamesdfow@users.noreply.github.com>
2026-03-24 21:08:12 -0600

    DOM manipulation, APIs, etc.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # JS-DOM-Manipulation
 
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jamesdfow/JS-DOM-Manipulation)
--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 // run `node index.js` in the terminal
+const firstName = document.getElementById("fname");
+const lastName = document.getElementById("lname");
+const button = document.querySelector("button");
+const weatherButton = document.querySelector("#fetchBtn");
+const weatherContainer = document.querySelector("#alertContainer")
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+button.addEventListener('click', function() {
+    if(button.innerText == "James button"){
+        button.innerText = "clicked";
+    }else{
+        button.innerText = "James button"
+    }
+})
+
+weatherButton.addEventListener("click", function() {
+    fetch("https://api.weather.gov/alerts?status=actual&message_type=alert&region_type=land&urgency=Immediate&severity=Extreme,Severe,Moderate&certainty=Observed&limit=2")
+    .then(response => response.json())
+    .then(data => {
+        const alerts = data.features;
+        weatherContainer.innerHTML = alerts.map(alert => `
+            <p><strong>${alert.properties.headline}</strong></p>
+            <p>${alert.properties.description}</p>
+            <hr>
+        `).join("");
+    });
+});
+
+
+
+
+
